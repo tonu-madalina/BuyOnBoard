d9e424af5a339f6388fbbc0ba77a1392ce67e33f
tonu-madalina <tonumadalina.d@gmail.com>
2026-07-27 23:42:39 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,57 @@
-console.log("BuyOnBoard a pornit cu succes!");
+const productsContainer = document.getElementById("products");
+
+function renderProducts(){
+
+productsContainer.innerHTML="";
+
+products.forEach(product=>{
+
+productsContainer.innerHTML += `
+
+<div class="product">
+
+<div class="counter">${product.qty}</div>
+
+<img src="${product.image}" alt="${product.name}">
+
+<div class="productName">
+
+${product.name}
+
+</div>
+
+<div class="bottomInfo">
+
+<div class="code">
+
+${product.code}
+
+</div>
+
+<div class="price">
+
+€${product.priceEUR.toFixed(2)}
+
+</div>
+
+</div>
+
+<div class="qty">
+
+<button>-</button>
+
+<span>${product.qty}</span>
+
+<button>+</button>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
+
+renderProducts();
