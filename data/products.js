8358a38046348b57131ce6b2700d7f80a70d0863
tonu-madalina <tonumadalina.d@gmail.com>
2026-07-27 23:16:56 +0300

    Add products database

---
+++ data/products.js
@@ -0,0 +1,27 @@
+const products = [
+
+{
+    id:1,
+    name:"Sandwich",
+    code:"001",
+    euro:5,
+    lei:100,
+    usd:6,
+    gbp:5,
+    image:"images/sandwich.jpg",
+    qty:0
+},
+
+{
+    id:2,
+    name:"Coffee",
+    code:"002",
+    euro:3,
+    lei:60,
+    usd:3,
+    gbp:3,
+    image:"images/coffee.jpg",
+    qty:0
+}
+
+];
