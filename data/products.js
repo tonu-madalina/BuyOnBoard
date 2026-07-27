18170d364b95522943c1611ba0964a7c370f6a7e
tonu-madalina <tonumadalina.d@gmail.com>
2026-07-27 23:42:06 +0300

    Update products.js

--- data/products.js
+++ data/products.js
@@ -1,27 +1,51 @@
 const products = [
 
 {
-    id:1,
-    name:"Sandwich",
-    code:"001",
-    euro:5,
-    lei:100,
-    usd:6,
-    gbp:5,
-    image:"images/sandwich.jpg",
-    qty:0
+id:1,
+name:"Sandwich",
+code:"001",
+priceEUR:5.00,
+priceMDL:100.00,
+priceUSD:5.80,
+priceGBP:4.50,
+image:"images/sandwich.png",
+qty:0
 },
 
 {
-    id:2,
-    name:"Coffee",
-    code:"002",
-    euro:3,
-    lei:60,
-    usd:3,
-    gbp:3,
-    image:"images/coffee.jpg",
-    qty:0
+id:2,
+name:"Coffee",
+code:"002",
+priceEUR:3.00,
+priceMDL:60.00,
+priceUSD:3.50,
+priceGBP:2.70,
+image:"images/coffee.png",
+qty:0
+},
+
+{
+id:3,
+name:"Tea",
+code:"003",
+priceEUR:2.50,
+priceMDL:50.00,
+priceUSD:2.90,
+priceGBP:2.20,
+image:"images/tea.png",
+qty:0
+},
+
+{
+id:4,
+name:"Water",
+code:"004",
+priceEUR:2.00,
+priceMDL:40.00,
+priceUSD:2.30,
+priceGBP:1.80,
+image:"images/water.png",
+qty:0
 }
 
 ];
